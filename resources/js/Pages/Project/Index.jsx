5787a6e0e97425b5dcf5c10cf5523fc22477d0a6
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2024-09-15 15:47:08 +0530

    Sorting by asc and desc

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -4,6 +4,8 @@ import Pagination from "@/Components/Pagination";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 import TextInput from "@/Components/TextInput";
 import SelectInput from "@/Components/SelectInput";
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/16/solid'
+import TableHeading from "@/Components/TableHeading";
 
 
 export default function Index({auth, projects, queryParams = null}) {
@@ -59,12 +61,47 @@ export default function Index({auth, projects, queryParams = null}) {
                             <table className = "w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead className= "text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                                 <tr className="text-nowrap">
-                                    <th onClick={e => handleSortChanged('id')} className = "px-3 py-3">ID</th>
+                                    <TableHeading
+                                        name="id"
+                                        sort_field={queryParams.sort_field}
+                                        sort_direction={queryParams.sort_direction}
+                                        handleSortChanged={handleSortChanged}
+                                      >
+                                        ID
+                                    </TableHeading>
                                     <th className = "px-3 py-3">Image</th>
-                                    <th onClick={e => handleSortChanged('name')} className = "px-3 py-3">Name</th>
-                                    <th onClick={e => handleSortChanged('status')} className = "px-3 py-3">Status</th>
-                                    <th onClick={e => handleSortChanged('created_at')} className = "px-3 py-3">Create Date</th>
-                                    <th onClick={e => handleSortChanged('due_date')} className = "px-3 py-3">Due Date</th>
+                                    <TableHeading
+                                        name="name"
+                                        sort_field={queryParams.sort_field}
+                                        sort_direction={queryParams.sort_direction}
+                                        handleSortChanged={handleSortChanged}
+                                      >
+                                        Name
+                                    </TableHeading>
+                                    <TableHeading
+                                        name="status"
+                                        sort_field={queryParams.sort_field}
+                                        sort_direction={queryParams.sort_direction}
+                                        handleSortChanged={handleSortChanged}
+                                      >
+                                        Status
+                                    </TableHeading>
+                                    <TableHeading
+                                        name="created_at"
+                                        sort_field={queryParams.sort_field}
+                                        sort_direction={queryParams.sort_direction}
+                                        handleSortChanged={handleSortChanged}
+                                      >
+                                        Created At
+                                    </TableHeading>
+                                    <TableHeading
+                                        name="due_date"
+                                        sort_field={queryParams.sort_field}
+                                        sort_direction={queryParams.sort_direction}
+                                        handleSortChanged={handleSortChanged}
+                                      >
+                                        Due Date
+                                    </TableHeading>
                                     <th className = "px-3 py-3">Created By</th>
                                     <th className = "px-3 py-3 text-right">Actions</th>
                                 </tr>
